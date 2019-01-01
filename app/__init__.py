966e2ca4bb73f0e67151aaf84b9ed3440a73e56b
Norbert Kwizera <norkans7@gmail.com>
2019-01-01 17:11:59 +0200

    Add SearchRecord model

--- app/__init__.py
+++ app/__init__.py
@@ -2,5 +2,6 @@ from app.factory import create_app
 from app.users import models
 from app.languages import models
 from app.entries import models
+from app.searches import models
 
 app = create_app()
